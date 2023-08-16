d4c77a684eaf72d838dcf3346a135fa2ad865ecc
nghiaved <nghiaved@gmail.com>
2023-08-16 17:51:39 +0700

    createGroupChat, renameGroup, removeFromGroup and addToGroup server

--- server/src/controllers/chat.controller.js
+++ server/src/controllers/chat.controller.js
@@ -59,12 +59,115 @@ const chatController = {
                     results = await userModel.populate(results, {
                         path: "latestMessage.sender",
                         select: "name image email",
-                    });
-                    res.status(200).send(results);
-                });
+                    })
+                    res.status(200).send(results)
+                })
         } catch (error) {
-            res.status(400);
-            throw new Error(error.message);
+            res.status(400)
+            throw new Error(error.message)
+        }
+    },
+
+    createGroupChat: async (req, res) => {
+        if (!req.body.users || !req.body.name) {
+            return res.status(400).send({ message: "Please Fill all the fields" })
+        }
+
+        var users = JSON.parse(req.body.users)
+
+        if (users.length < 2) {
+            return res
+                .status(400)
+                .send("More than 2 users are required to form a group chat")
+        }
+
+        users.push(req.user)
+
+        try {
+            const groupChat = await chatModel.create({
+                chatName: req.body.name,
+                users: users,
+                isGroupChat: true,
+                groupAdmin: req.user,
+            })
+
+            const fullGroupChat = await chatModel.findOne({ _id: groupChat._id })
+                .populate("users", "-password")
+                .populate("groupAdmin", "-password")
+
+            res.status(200).json(fullGroupChat)
+        } catch (error) {
+            res.status(400)
+            throw new Error(error.message)
+        }
+    },
+
+    renameGroup: async (req, res) => {
+        const { chatId, chatName } = req.body
+
+        const updatedChat = await chatModel.findByIdAndUpdate(
+            chatId,
+            {
+                chatName: chatName,
+            },
+            {
+                new: true,
+            }
+        )
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+
+        if (!updatedChat) {
+            res.status(404)
+            throw new Error("Chat Not Found")
+        } else {
+            res.json(updatedChat)
+        }
+    },
+
+    removeFromGroup: async (req, res) => {
+        const { chatId, userId } = req.body
+
+        const removed = await chatModel.findByIdAndUpdate(
+            chatId,
+            {
+                $pull: { users: userId },
+            },
+            {
+                new: true,
+            }
+        )
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+
+        if (!removed) {
+            res.status(404)
+            throw new Error("Chat Not Found")
+        } else {
+            res.json(removed)
+        }
+    },
+
+    addToGroup: async (req, res) => {
+        const { chatId, userId } = req.body;
+
+        const added = await chatModel.findByIdAndUpdate(
+            chatId,
+            {
+                $push: { users: userId },
+            },
+            {
+                new: true,
+            }
+        )
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password");
+
+        if (!added) {
+            res.status(404);
+            throw new Error("Chat Not Found");
+        } else {
+            res.json(added);
         }
     }
 }
--- server/src/routes/chat.route.js
+++ server/src/routes/chat.route.js
@@ -8,6 +8,10 @@ const router = express.Router()
 const chatRoute = app => {
     router.post('/', protect, chatController.accessChat)
     router.get('/', protect, chatController.fetchChats)
+    router.post('/group', protect, chatController.createGroupChat)
+    router.put('/grouprename', protect, chatController.renameGroup)
+    router.put('/groupremove', protect, chatController.removeFromGroup)
+    router.put('/groupadd', protect, chatController.addToGroup)
 
     return app.use('/api/chat', router)
 }
